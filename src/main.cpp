c4da7019b5873d1ff356a5c74d0aabb3be58c9ee
Sander <sander@sanderf.nl>
2022-09-03 12:35:38 +0300

    read env. file when in CMake Debug mode

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,33 @@ int main(int argc, char *argv[]) {
   Q_INIT_RESOURCE(chatty);
   Q_INIT_RESOURCE(irssi);
 
+#ifdef DEBUG
+  // For remote debugging (QEMU) with CLion, the environment variables need
+  // to be correctly set such that e.g. dbus will work. We can execute
+  // this hack to dump the environ to a file, then reads it below.
+  // This snippet runs when `-D CMAKE_DEBUG_TYPE=Debug`
+
+  // /bin/sh -c 'nohup /tmp/tmp.fCTLrJeUgW/cmake-build-debug/bin/conversations >/dev/null 2>&1 &'; sleep 2; cat "/proc/`pidof conversations`/environ" | tr "\0" "\n" > /home/user/env.sh; kill -9 "`pidof conversations`"
+  setuid(1000);
+  QString path_env_file = "/home/user/env.sh";
+  qDebug() << "trying to read ENV from '" << path_env_file << "', if it exists";
+  auto env_file = Utils::fileOpen(path_env_file);
+  for(auto &line: Utils::barrayToString(env_file).split("\n")) {
+    line = line.replace("export ", "");
+    int pos = line.indexOf("=");
+    auto key = line.left(pos);
+    auto val = line.remove(0, pos + 1);
+
+    if(val.startsWith("\""))
+      val = val.mid(1);
+    if(val.endsWith("\""))
+      val = val.mid(0, val.length() - 1);
+
+    if(val.isEmpty() || key.isEmpty()) continue;
+    qputenv(key.toStdString().c_str(), val.toStdString().c_str());
+  }
+#endif
+
   intl("conversations-ui");
   qputenv("QML_DISABLE_DISK_CACHE", "1");
 
