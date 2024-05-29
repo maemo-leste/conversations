e97fc778a4f348b5f834f333aad4f19cd17e5f03
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2024-05-29 17:25:50 +0300

    lib/tp: mark own incomming messages as outgoing
    Add some logic that tries to recognise scrollback own messages
    
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

--- src/lib/tp.cpp
+++ src/lib/tp.cpp
@@ -458,8 +458,23 @@ void TelepathyAccount::onMessageReceived(const Tp::ReceivedMessage &message, con
     auto remote_alias = message.sender()->alias();
     auto text = message.text().toLocal8Bit();
 
+    /* from tp-glib specs:
+       TpContact::identifier - The contact's identifier in the instant messaging
+           protocol (e.g. XMPP JID, SIP URI, AOL screenname or IRC nick -
+           whatever the underlying protocol uses to identify a user)
+       TpAccount::normalized-name  - The normalized form of the user's own
+           unique identifier on this protocol. For example, on XMPP accounts
+           this is the user's JID; on ICQ this is the user's UIN; and so on.
+
+       So, my understanding is that if those match, and message is scrollback,
+       then this is a message from me, coming as a scrollback from another
+       device
+    */
+    bool outgoing = message.isScrollback() && acc->normalizedName() == remote_uid;
+
     qDebug() << "log_event";
-    log_event(epoch, text, false, channel, remote_uid, remote_alias);
+
+    log_event(epoch, text, outgoing, channel, remote_uid, remote_alias);
 }
 
 /* When we have managed to send a message */
