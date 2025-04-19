41e75ac1b8feeaf0125a2ea703366c8fa29f55a9
Sander <sander@sanderf.nl>
2025-04-19 09:37:13 +0300

    conversations_slim/chatwindow: QGrabScrollTextEdit.h, scrolling with grab gestures + kinetic scrolling + text selecting

--- src/lib/QGrabScrollTextEdit.h
+++ src/lib/QGrabScrollTextEdit.h
@@ -3,80 +3,31 @@
 #include <QScrollBar>
 #include <QTouchEvent>
 
+#include <QTextEdit>
+#include <QScroller>
+#include <QScrollerProperties>
+
 class QGrabScrollTextEdit : public QTextEdit {
 public:
-  explicit QGrabScrollTextEdit(QWidget *parent = nullptr) :
-    QTextEdit(parent), isDragging(false) {
-    setTextInteractionFlags(Qt::NoTextInteraction);
+  explicit QGrabScrollTextEdit(QWidget *parent = nullptr)
+      : QTextEdit(parent) {
+    // setTextInteractionFlags(Qt::NoTextInteraction);
     setAttribute(Qt::WA_AcceptTouchEvents);
+    QScroller *scroller = QScroller::scroller(this->viewport());
+    QScrollerProperties props = scroller->scrollerProperties();
+
+    // no workie
+    // QVariant overshootPolicy = QVariant::fromValue<QScrollerProperties::OvershootPolicy>(
+    //     QScrollerProperties::OvershootAlwaysOff
+    // );
+
+    props.setScrollMetric(QScrollerProperties::DragVelocitySmoothingFactor, 0.6);
+    props.setScrollMetric(QScrollerProperties::MinimumVelocity, 0.0);
+    props.setScrollMetric(QScrollerProperties::MaximumVelocity, 1.5);
+    props.setScrollMetric(QScrollerProperties::DecelerationFactor, 0.15);
+    scroller->setScrollerProperties(props);
+
+    QScroller::grabGesture(this->viewport(), QScroller::TouchGesture);
+    QScroller::grabGesture(this->viewport(), QScroller::LeftMouseButtonGesture);
   }
-
-protected:
-  bool event(QEvent *event) override {
-    if (event->type() == QEvent::TouchBegin ||
-        event->type() == QEvent::TouchUpdate ||
-        event->type() == QEvent::TouchEnd) {
-      return handleTouchEvent(static_cast<QTouchEvent *>(event));
-    }
-    return QTextEdit::event(event);
-  }
-
-  bool handleTouchEvent(const QTouchEvent *event) {
-    if (event->touchPoints().count() == 1) {
-      const QTouchEvent::TouchPoint &point = event->touchPoints().first();
-
-      if (event->type() == QEvent::TouchBegin) {
-        isDragging = true;
-        dragStartPos = point.pos().toPoint();
-        return true;
-      } else if (event->type() == QEvent::TouchUpdate && isDragging) {
-        const QPoint delta = point.pos().toPoint() - dragStartPos;
-        verticalScrollBar()->setValue(verticalScrollBar()->value() - delta.y());
-        horizontalScrollBar()->setValue(horizontalScrollBar()->value() - delta.x());
-        dragStartPos = point.pos().toPoint();
-        return true;
-      } else if (event->type() == QEvent::TouchEnd) {
-        isDragging = false;
-        return true;
-      }
-    }
-    return false;
-  }
-
-  void mousePressEvent(QMouseEvent *event) override {
-    if (event->button() == Qt::LeftButton) {
-      isDragging = true;
-      dragStartPos = event->pos();
-      setCursor(Qt::ClosedHandCursor);
-      event->accept();
-    } else {
-      QTextEdit::mousePressEvent(event);
-    }
-  }
-
-  void mouseMoveEvent(QMouseEvent *event) override {
-    if (isDragging) {
-      QPoint delta = event->pos() - dragStartPos;
-      verticalScrollBar()->setValue(verticalScrollBar()->value() - delta.y());
-      horizontalScrollBar()->setValue(horizontalScrollBar()->value() - delta.x());
-      dragStartPos = event->pos();
-      event->accept();
-    } else {
-      QTextEdit::mouseMoveEvent(event);
-    }
-  }
-
-  void mouseReleaseEvent(QMouseEvent *event) override {
-    if (event->button() == Qt::LeftButton && isDragging) {
-      isDragging = false;
-      setCursor(Qt::ArrowCursor);
-      event->accept();
-    } else {
-      QTextEdit::mouseReleaseEvent(event);
-    }
-  }
-
-private:
-  bool isDragging;
-  QPoint dragStartPos;
 };
