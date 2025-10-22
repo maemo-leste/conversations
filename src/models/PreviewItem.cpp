32614d36972cdf7d8e576c374c1fc6f1310f326e
Sander <sander@sanderf.nl>
2025-10-22 18:06:41 +0300

    qml/preview: dynamic heights for inline images

--- src/models/PreviewItem.cpp
+++ src/models/PreviewItem.cpp
@@ -28,22 +28,22 @@ int PreviewItem::height(const unsigned int relative_width) const {
   if (state == State::Idle)
     return 0;
 
-  bool inlineImages = config()->get(ConfigKeys::LinkPreviewImageEnabled).toBool();
+  // dynamic heights for images
+  const bool inlineImages = config()->get(ConfigKeys::LinkPreviewImageEnabled).toBool();
   if (displayType == DisplayType::Image && inlineImages) {
-    return 120;
-  }
+    auto size = Conversations::instance()->previewImageProvider->getSize(filePath);
+    auto _width = size.width();
+    auto _height = size.height();
 
-  // @TODO: dynamic heights, lets do static for now
-  // if (relative_width != 0 &&
-  //     itemType == ImageType &&
-  //     displayType == ImageDisplay) {
-  //
-  //   auto size = Conversations::instance()->previewImageProvider->getSize(filePath);
-  //   if (size.width() > 0 && size.height() > 0) {
-  //     auto _height = size.height() * relative_width / size.width();
-  //     return _height;
-  //   }
-  // }
+    if (_width > 0 && _height > 0) {
+      const double aspect = static_cast<double>(_width) / _height;
+      if (aspect >= 1.2)
+        return 160;
+      if (aspect <= 0.85)
+        return 280;
+      return 220;
+    }
+  }
 
   return m_defaultHeight;
 }
